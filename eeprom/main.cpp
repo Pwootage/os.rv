9f32dfbd378ab8218b4fa82ca9718547aaf76e29
Pwootage <pwootage@gmail.com>
2020-07-14 22:29:43 -0600

    Begin the process of actually calling a component

--- eeprom/main.cpp
+++ eeprom/main.cpp
@@ -7,17 +7,18 @@
 #define BI_INT32 0x03
 #define BI_INT64 0x04
 #define BI_INT128 0x05
-#define BI_ARRAY 0x06
+#define BI_BYTES 0x06
 #define BI_OBJECT 0x07
 #define BI_END 0xFF
 
-#define COMPONENT_ID_INVOKE 0x01
+#define COMPONENT_ID_INVOKE 0x00
 #define COMPONENT_ID_LIST 0x01
 
-constexpr volatile uint8_t * const component_fifo = (volatile uint8_t *)0x10001000;
-constexpr volatile uint8_t * const eeprom = (volatile uint8_t *)0x20000000;
-constexpr volatile uint8_t * const eeprom_data = (volatile uint8_t *)0x20010000;
-const char *KERNEL_PATH = "/kernel";
+volatile uint8_t * const component_fifo = (volatile uint8_t *)0x10001000;
+volatile uint8_t * const component_fifo_write_ready = (volatile uint8_t *)0x10001002;
+volatile uint8_t * const eeprom = (volatile uint8_t *)0x20000000;
+volatile uint8_t * const eeprom_data = (volatile uint8_t *)0x20010000;
+const char *KERNEL_PATH = "/kernel\0";
 
 struct UUID {
     union {
@@ -26,7 +27,7 @@ struct UUID {
             uint64_t lo;
         };
         char value[16];
-    }
+    };
 };
 
 void write_fifo(int value, uint32_t size) {
@@ -45,15 +46,30 @@ void write_fifo(const void *ptr, uint32_t size) {
 
 void write_fifo_int8(uint8_t value) {
     write_fifo(BI_INT8, 1);
-    write_fifo(COMPONENT_ID_INVOKE, 1);
+    write_fifo(value, 1);
 }
-void write_fifo_int32(uint8_t value) {
-    write_fifo(BI_INT8, 1);
-    write_fifo(COMPONENT_ID_INVOKE, 1);
+void write_fifo_int32(uint32_t value) {
+    write_fifo(BI_INT32, 1);
+    write_fifo(value, 4);
 }
 void write_fifo_uuid(const UUID &value) {
     write_fifo(BI_INT128, 1);
-    write_fifo(value.value, 16);
+    write_fifo(&value.value, 16);
+}
+void write_fifo_string(const char *str) {
+    // Figure out length
+    int len = 0;
+    while (str[len] != 0) len++;
+    write_fifo(BI_BYTES, 1);
+    write_fifo(len, 4);
+    write_fifo(str, len);
+}
+void write_fifo_end() {
+    write_fifo(BI_END, 1);
+}
+void write_fifo_ready() {
+    *component_fifo_write_ready = 1;
+    asm("ebreak");
 }
 
 void memcpy(void *dest, const void *src, uint32_t size) {
@@ -67,24 +83,32 @@ void memcpy(void *dest, const void *src, uint32_t size) {
 int fopen(const UUID &target_fs, const char *path) {
     // Invoke
 
-    write_fifo()
+    write_fifo_int8(COMPONENT_ID_INVOKE);
+    write_fifo_uuid(target_fs);
+    write_fifo_string("open\0");
+    write_fifo_string(path);
+    write_fifo_end();
+    write_fifo_ready(); 
 }
 
-bool read_kernel(char *boot_id) {
-    
+bool read_kernel(const UUID &target_fs) {
+    int handle = fopen(target_fs, KERNEL_PATH);
+
+    return handle > 0;
 } 
 
 extern "C" {
     int main() {
         // the EEPROM is mapped directly to memory; read out the boot partition ID
-        char boot_id[16];
-        for (int i = 0; i < sizeof(boot_id); i++) {
-            boot_id[i] = eeprom_data[i];
+        UUID boot_id;
+        for (int i = 0; i < sizeof(boot_id.value); i++) {
+            boot_id.value[i] = eeprom_data[i];
         }
         // Attempt to read /kernel
         bool res = read_kernel(boot_id);
 
         // Hang
         while (1) {}
+        return 1;
     }
 }
